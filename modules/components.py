eed65badb2480b201d44a29a47fad1711a422066
katiaviene <katiacookiefairy@gmail.com>
2024-02-25 17:20:13 +0200

    deb

--- modules/components.py
+++ modules/components.py
@@ -23,7 +23,7 @@ class Widgets:
                             width="100%"
                             height="800"
                             frameborder="0"
-                            scrolling="no">
+                            scrolling="yes">
                         </iframe>
                     '''
 
